25f17ab92e0b298a3beb9dd4f0c995f0a6be0c6b
Saarth Rajan <s25rajan@uwaterloo.ca>
2024-10-05 15:54:23 -0400

    removed streamlit branding

--- float-n-pose.py
+++ float-n-pose.py
@@ -13,7 +13,7 @@ st.set_page_config(
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-local_css("styles/styles.css")
+local_css("Styles/styles.css")
 
 st.title("Float n Pose")
 
